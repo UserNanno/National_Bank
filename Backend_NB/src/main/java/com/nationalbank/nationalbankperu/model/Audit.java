5008f2e22ee4ce2d29e13d453bba2c16bd095e5e
UserNanno <juan.canecillas@unmsm.edu.pe>
2025-02-23 12:55:31 -0500

    feat: crea la entidad Audit con timestamps automáticos

---
+++ Backend_NB/src/main/java/com/nationalbank/nationalbankperu/model/Audit.java
@@ -0,0 +1,30 @@
+package com.nationalbank.nationalbankperu.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Embeddable
+@Getter
+@Setter
+public class Audit {
+
+    @Column(name = "created_at", updatable = false, nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now(); // Asegura que no sea nulo en la creación
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+}
