6453e7d132a7e03e9e4ac17e979f60fb00bd231a
UserNanno <juan.canecillas@unmsm.edu.pe>
2025-02-23 12:49:18 -0500

    mejora punto de entrada con configuración de escaneo de componentes

--- Backend_NB/src/main/java/com/nationalbank/nationalbankperu/NationalbankperuApplication.java
+++ Backend_NB/src/main/java/com/nationalbank/nationalbankperu/NationalbankperuApplication.java
@@ -1,4 +1,14 @@
 package com.nationalbank.nationalbankperu;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = "com.nationalbank") // Escaneo explícito de componentes
 public class NationalbankperuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NationalbankperuApplication.class, args);
+    }
 }
