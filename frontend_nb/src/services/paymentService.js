7ab03567c60ac780e46e3f7efd7eeddf85236fb3
UserNanno <juan.canecillas@unmsm.edu.pe>
2025-02-23 23:31:56 -0500

    refactor(styles, app): Diminución de la redundancia y modularizacion de estilos

--- frontend_nb/src/services/paymentService.js
+++ frontend_nb/src/services/paymentService.js
@@ -1,3 +1,5 @@
+import { getLocalData } from '../utils/localStorageUtils';
+
 const API_BASE_URL = 'http://localhost:8080/api/service-payments';
 
 export async function fetchAvailableServices() {
--- frontend_nb/src/services/transactionService.js
+++ frontend_nb/src/services/transactionService.js
@@ -1,3 +1,5 @@
+import { getLocalData } from '../utils/localStorageUtils';
+
 const API_BASE_URL = 'http://localhost:8080/api/transactions';
 
 export async function makeTransfer({ fromAccount, toAccount, amount }) {
