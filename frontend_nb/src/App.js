176b3315f5f44b9cf067b16b1b84b558851b0bc4
UserNanno <juan.canecillas@unmsm.edu.pe>
2025-02-23 20:25:17 -0500

    refactor(app): Agregamos la estructura y optimización de rutas

--- frontend_nb/src/App.js
+++ frontend_nb/src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// Importamos componentes generales
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+// Importamos las páginas
+import Contacto from "./pages/Contacto";
+import GestionarCuenta from "./pages/GestionarCuenta";
+import Inicio from "./pages/Inicio";
+import Menu from "./pages/Menu";
+import Nosotros from "./pages/Nosotros";
+import PagoServicios from "./pages/PagoServicios";
+import Perfil from "./pages/Perfil";
+import Registro from "./pages/Registro";
+import Transferencias from "./pages/Transferencias";
+
+// Importamos los estilos globales
+import "./styles/global.css";
+import "./styles/responsive.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Inicio />} />
+        <Route path="/nosotros" element={<Nosotros />} />
+        <Route path="/contacto" element={<Contacto />} />
+        <Route path="/transferencias" element={<Transferencias />} />
+        <Route path="/pago-servicios" element={<PagoServicios />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/registro" element={<Registro />} />
+        <Route path="/gestionar-cuenta" element={<GestionarCuenta />} />
+        <Route path="/perfil" element={<Perfil />} />
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
